2872d111c8b3a7a72800a1bfa70ec25e2e2edab6
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-21 16:45:02 +0530

    Added:calculetor by using delegates

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Delegates
+{
+    public delegate void math(int a, int b);
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while(true)
+            { 
+              Console.WriteLine("enter symbol acording to opration + - / *");
+              string c=Console.ReadLine();
+              Console.Write("enter number 1 :");
+                int a=int.Parse(Console.ReadLine());
+                Console.Write("enter number 2 :");
+                int b = int.Parse(Console.ReadLine());
+
+
+                char check = c[0];
+                switch (check)
+                {
+                    case '+':
+                        math obj = new math(delegates.add);
+                        obj.Invoke(a, b);
+
+                        break;
+
+                    case '-':
+                        obj = new math(delegates.substraction);
+                        obj.Invoke(a, b);
+                        break;
+
+                    case '*':
+                        obj = new math(delegates.multiplication);
+                        obj.Invoke(a, b);
+
+                        break;
+
+                    case '/':
+                        obj = new math(delegates.division);
+                        obj.Invoke(a, b);
+                    
+                        break;
+
+                    default:
+                        Console.WriteLine("enter right input");
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ Delegates/Delegates/for_delegates.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+
+    class delegates
+    {
+        internal static void  add(int a, int b) 
+        {
+            var result = a + b;
+            Console.WriteLine("Result:"+result);
+        
+        }
+        internal static void multiplication(int a, int b)
+        {
+            var result = a * b;
+            Console.WriteLine("Result:"+result);
+
+
+        }
+        internal static void substraction(int a, int b)
+        {
+            var result = a - b;
+            Console.WriteLine("Result:"+result);
+
+
+        }
+
+        internal static void division(int a, int b)
+        {
+            var result = a / b;
+            Console.WriteLine("Result:"+result);
+
+
+        }
+
+    }
+}
